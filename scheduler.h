220e4a2b40838703f7e09007276b3b966e0453cd
Nadir <yakoubinadir74@gmail.com>
2026-04-15 15:11:44 +0100

    Add function prototypes in scheduler.h
    Added function prototypes for process management.

--- scheduler.h
+++ scheduler.h
@@ -15,7 +15,8 @@ typedef struct Process{
     struct Process *precedent;// un pointeur qui pointe vers le process precedent
 } Process ;
 
-// Prototypes des fonctions (on les implémentera plus tard)
+// Prototypes des fonctions
+
 Process* create_process(int id, int priority, int time);
 
 Process* ajouter_process_a_la_fin(Process *tete, Process *nv_p);
